f6da44720bbc3ee3c29fcf2ce8f94c2667c51eaa
Albert <clivethology24@gmail.com>
2023-11-11 14:22:05 +0800

    Home Design

--- app/src/main/java/com/example/ricedoc/PredictionFragment.java
+++ app/src/main/java/com/example/ricedoc/PredictionFragment.java
@@ -1,39 +1,102 @@
 package com.example.ricedoc;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import androidx.fragment.app.Fragment;
 
 public class PredictionFragment extends Fragment {
     private WebView webView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_prediction, container, false);
 
         webView = view.findViewById(R.id.webview);
-
-        // Configure WebView settings
         WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true); // Enable JavaScript if needed
-
-        // Set cache mode to allow using cache if available
+        webSettings.setJavaScriptEnabled(true);
         webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
 
-        // Set a WebViewClient to handle page navigation
-        webView.setWebViewClient(new WebViewClient());
+        // Check network connectivity
+        if (isNetworkAvailable()) {
+            webView.setWebViewClient(new MyWebViewClient());
+            webView.setWebChromeClient(new MyWebChromeClient());
 
-        // Load a web page
-        webView.loadUrl("https://moldy24.pythonanywhere.com"); // Replace with your
+            // Load a web page
+            webView.loadUrl("https://moldy24.pythonanywhere.com");
+        } else {
+            showNoInternetDialog();
+        }
 
         return view;
+    }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) requireActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    private void showNoInternetDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle("No Internet Connection");
+        builder.setMessage("Please enable mobile data or Wi-Fi in settings.");
+        builder.setPositiveButton("Go to Settings", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(android.provider.Settings.ACTION_SETTINGS);
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                requireActivity().onBackPressed();
+            }
+        });
+        builder.show();
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            super.onReceivedError(view, request, error);
+            showErrorDialog();
+        }
+    }
+
+    private class MyWebChromeClient extends WebChromeClient {
+        // Handle progress, loading dialog, etc. if needed
+    }
+
+    private void showErrorDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle("Error Loading Page");
+        builder.setMessage("There was an error loading the web page. Please try again.");
+        builder.setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                webView.reload();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                requireActivity().finish(); // Close the app or handle accordingly
+            }
+        });
+        builder.show();
     }
 }
