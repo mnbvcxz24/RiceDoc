b3a353d248c459918595e0645cf29e0382835629
Albert <clivethology24@gmail.com>
2023-11-12 17:11:53 +0800

    Books Transfer

---
+++ app/src/main/java/com/example/ricedoc/BookCard2.java
@@ -0,0 +1,14 @@
+package com.example.ricedoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookCard2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_card2);
+    }
+}
