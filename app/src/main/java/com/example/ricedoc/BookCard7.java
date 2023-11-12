0c4ade5a3dbe1bb6dfdb8aa35d8d4d9be4bf7379
Albert <clivethology24@gmail.com>
2023-11-12 23:20:22 +0800

    Icon Logo

---
+++ app/src/main/java/com/example/ricedoc/BookCard7.java
@@ -0,0 +1,14 @@
+package com.example.ricedoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookCard7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_card7);
+    }
+}
