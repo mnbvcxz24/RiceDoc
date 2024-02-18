2739a8fe7c77ca89e3417273a141c597653f8dc1
Albert <clivethology24@gmail.com>
2024-02-18 13:28:14 +0800

    feb 18

--- app/src/main/java/com/capstone/ricedoc/DialogLocation.java
+++ app/src/main/java/com/capstone/ricedoc/DialogLocation.java
@@ -1,6 +1,7 @@
 package com.capstone.ricedoc;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -10,23 +11,74 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class DialogLocation extends DialogFragment {
+    private Set<String> barangaySet = new HashSet<>();
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(getActivity());
 
-        String [] barangays = getActivity().getResources().getStringArray(R.array.barangay);
+        // Get the barangay list in the firestore
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+        CollectionReference collectionReference = firebaseFirestore.collection("rice_production_data");
 
-        builder.setTitle("Choose Barangay");
-        builder.setItems(barangays, new DialogInterface.OnClickListener() {
+        AlertDialog dialog1 = builder1.create();
+
+        collectionReference.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                            String barangay = document.getString("Barangay");
+                            barangaySet.add(barangay);
+                        }
+                        showSecondDialog();
+                        dialog1.dismiss();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.err.println("ERROR: " + e);
+                    }
+                });
+
+        // Return null to avoid creating an empty AlertDialog here
+        return dialog1;
+    }
+
+    private void showSecondDialog() {
+        AlertDialog.Builder builder2 = new AlertDialog.Builder(getActivity());
+        String[] barangays = barangaySet.toArray(new String[0]);
+
+        builder2.setTitle("Choose Barangay");
+        builder2.setItems(barangays, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int i) {
-                Toast.makeText(getActivity(), "Barangay : " + barangays[i], Toast.LENGTH_SHORT).show();
+            public void onClick(DialogInterface dialog2, int i) {
+                System.out.println("Barangay : " + barangays[i]);
+                //Toast.makeText(getActivity(), "Barangay : " + barangays[i], Toast.LENGTH_SHORT).show();
             }
         });
 
-        return builder.create();
+        AlertDialog dialog2 = builder2.create();
+        dialog2.show();
     }
 }
+
+
--- app/src/main/java/com/capstone/ricedoc/HistoryFragment.java
+++ app/src/main/java/com/capstone/ricedoc/HistoryFragment.java
@@ -2,12 +2,20 @@ package com.capstone.ricedoc;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+
+import com.google.firebase.database.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class HistoryFragment extends Fragment {
 
@@ -18,11 +26,35 @@ public class HistoryFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_history, container, false);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("recent_scan");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Set<String> barangaySet = new HashSet<>();
+
+                for (DataSnapshot entrySnapshot : snapshot.getChildren()) {
+                    String barangay = entrySnapshot.child("barangay").getValue(String.class);
+                    barangaySet.add(barangay);
+                }
+
+                System.out.println("Unique Barangay :"+barangaySet);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.err.println("Error "+error.getMessage());
+            }
+        });
+
+        ArrayList<String> barangayList = new ArrayList<>();
+
         btnLocation = view.findViewById(R.id.btnLocation);
         btnLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new DialogLocation().show(getActivity().getSupportFragmentManager(), "dialogLocation");
+                new DialogLocation().show(getChildFragmentManager(), "dialogLocation");
             }
         });
         return view;
