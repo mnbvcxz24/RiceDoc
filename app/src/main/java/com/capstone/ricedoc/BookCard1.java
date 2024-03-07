b5c276b92145afb5dd9116c9270e712f7421bde4
Albert <clivethology24@gmail.com>
2024-03-07 12:45:48 +0800

    mar 7

--- app/src/main/java/com/capstone/ricedoc/BookCard1.java
+++ app/src/main/java/com/capstone/ricedoc/BookCard1.java
@@ -3,14 +3,23 @@ package com.capstone.ricedoc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.capstone.ricedoc.R;
 
 public class BookCard1 extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_card1);
+
+        ImageButton btnBack = this.findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
--- app/src/main/java/com/capstone/ricedoc/BookCard4.java
+++ app/src/main/java/com/capstone/ricedoc/BookCard4.java
@@ -3,6 +3,8 @@ package com.capstone.ricedoc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.capstone.ricedoc.R;
 
@@ -12,5 +14,13 @@ public class BookCard4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_card4);
+
+        ImageButton btnBack = this.findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
